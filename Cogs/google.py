586503a8c30756bba09f5b9e4be0b07b54713246
Winston-Lu <wlu2268@gmail.com>
2020-09-26 16:43:12 -0700

    fixed errors with translate

--- Cogs/google.py
+++ Cogs/google.py
@@ -17,7 +17,7 @@ class Google(commands.Cog):
             em.description = f'Translates TO_TRANSLATE to english using google translate'
             em.add_field(name="Example", value="/translate Je ne sais pas | /translate こんにちは世界", inline=False)
             em.add_field(name="Aliases", value="/t", inline=False)
-            em.add_field(name="Supported Languages", value="af: afrikaans, sq: albanian, am: amharic, ar: arabic, hy: armenian, az: azerbaijani, eu: basque, be: belarusian, bn: bengali, bs: bosnian, bg: bulgarian, ca: catalan, ceb: cebuano, ny: chichewa, zh-cn: chinese (simplified), zh-tw: chinese (traditional), co: corsican, hr: croatian, cs: czech, da: danish, nl: dutch, en: english, eo: esperanto, et: estonian, tl: filipino, fi: finnish, fr: french, fy: frisian, gl: galician, ka: georgian, de: german, el: greek, gu: gujarati, ht: haitian creole, ha: hausa, haw: hawaiian, iw: hebrew, he: hebrew, hi: hindi, hmn: hmong, hu: hungarian, is: icelandic, ig: igbo, id: indonesian, ga: irish, it: italian, ja: japanese, jw: javanese, kn: kannada, kk: kazakh, km: khmer, ko: korean, ku: kurdish (kurmanji)", inline=False)
+            em.add_field(name="Supported Languages", value="af: afrikaans, sq: albanian, am: amharic, ar: arabic, hy: armenian, az: azerbaijani, eu: basque, be: belarusian, bn: bengali, bs: bosnian, bg: bulgarian, ca: catalan, ceb: cebuano, ny: chichewa, zh-cn: chinese_simplified, zh-tw: chinese_traditional, co: corsican, hr: croatian, cs: czech, da: danish, nl: dutch, en: english, eo: esperanto, et: estonian, tl: filipino, fi: finnish, fr: french, fy: frisian, gl: galician, ka: georgian, de: german, el: greek, gu: gujarati, ht: haitian creole, ha: hausa, haw: hawaiian, iw: hebrew, he: hebrew, hi: hindi, hmn: hmong, hu: hungarian, is: icelandic, ig: igbo, id: indonesian, ga: irish, it: italian, ja: japanese, jw: javanese, kn: kannada, kk: kazakh, km: khmer, ko: korean, ku: kurdish (kurmanji)", inline=False)
             em.add_field(name="\u200b", value="ky: kyrgyz, lo: lao, la: latin, lv: latvian, lt: lithuanian, lb: luxembourgish, mk: macedonian, mg: malagasy, ms: malay, ml: malayalam, mt: maltese, mi: maori, mr: marathi, mn: mongolian, my: myanmar (burmese), ne: nepali, no: norwegian, or: odia, ps: pashto, fa: persian, pl: polish, pt: portuguese, pa: punjabi, ro: romanian, ru: russian, sm: samoan, gd: scots gaelic, sr: serbian, st: sesotho, sn: shona, sd: sindhi, si: sinhala, sk: slovak, sl: slovenian, so: somali, es: spanish, su: sundanese, sw: swahili, sv: swedish, tg: tajik, ta: tamil, te: telugu, th: thai, tr: turkish, uk: ukrainian, ur: urdu, ug: uyghur, uz: uzbek, vi: vietnamese, cy: welsh, xh: xhosa, yi: yiddish, yo: yoruba, zu: zulu", inline=False)
             em.color = 0x22BBFF
             await ctx.send(embed=em)
@@ -28,7 +28,7 @@ class Google(commands.Cog):
             prediction = translator.detect(query)
             em = discord.Embed()
             em.title = f'Translation'
-            em.description = f'Source language: {googletrans.LANGUAGES.get(prediction.lang).title()}\nConfidence: {prediction.confidence*100}%'
+            em.description = f'Source language: {googletrans.LANGUAGES.get(prediction.lang)}\nConfidence: {prediction.confidence*100}%'
             if(len(translated.text) > 1020): #1024 character limit
                 em.add_field(name="Translation", value=translated.text[:1020], inline=False)
                 em.add_field(name="\u200b", value=translated.text[1020:], inline=False)
@@ -45,13 +45,17 @@ class Google(commands.Cog):
             em.description = f'Translates TO_TRANSLATE to RESULT_LANG using google translate'
             em.add_field(name="Example", value="/translate english Je ne sais pas | /translate ar hello world!", inline=False)
             em.add_field(name="Aliases", value="/t2", inline=False)
-            em.add_field(name="Supported Languages", value="af: afrikaans, sq: albanian, am: amharic, ar: arabic, hy: armenian, az: azerbaijani, eu: basque, be: belarusian, bn: bengali, bs: bosnian, bg: bulgarian, ca: catalan, ceb: cebuano, ny: chichewa, zh-cn: chinese (simplified), zh-tw: chinese (traditional), co: corsican, hr: croatian, cs: czech, da: danish, nl: dutch, en: english, eo: esperanto, et: estonian, tl: filipino, fi: finnish, fr: french, fy: frisian, gl: galician, ka: georgian, de: german, el: greek, gu: gujarati, ht: haitian creole, ha: hausa, haw: hawaiian, iw: hebrew, he: hebrew, hi: hindi, hmn: hmong, hu: hungarian, is: icelandic, ig: igbo, id: indonesian, ga: irish, it: italian, ja: japanese, jw: javanese, kn: kannada, kk: kazakh, km: khmer, ko: korean, ku: kurdish (kurmanji)", inline=False)
+            em.add_field(name="Supported Languages", value="af: afrikaans, sq: albanian, am: amharic, ar: arabic, hy: armenian, az: azerbaijani, eu: basque, be: belarusian, bn: bengali, bs: bosnian, bg: bulgarian, ca: catalan, ceb: cebuano, ny: chichewa, zh-cn: chinese_simplified, zh-tw: chinese_traditional, co: corsican, hr: croatian, cs: czech, da: danish, nl: dutch, en: english, eo: esperanto, et: estonian, tl: filipino, fi: finnish, fr: french, fy: frisian, gl: galician, ka: georgian, de: german, el: greek, gu: gujarati, ht: haitian creole, ha: hausa, haw: hawaiian, iw: hebrew, he: hebrew, hi: hindi, hmn: hmong, hu: hungarian, is: icelandic, ig: igbo, id: indonesian, ga: irish, it: italian, ja: japanese, jw: javanese, kn: kannada, kk: kazakh, km: khmer, ko: korean, ku: kurdish (kurmanji)", inline=False)
             em.add_field(name="\u200b", value="ky: kyrgyz, lo: lao, la: latin, lv: latvian, lt: lithuanian, lb: luxembourgish, mk: macedonian, mg: malagasy, ms: malay, ml: malayalam, mt: maltese, mi: maori, mr: marathi, mn: mongolian, my: myanmar (burmese), ne: nepali, no: norwegian, or: odia, ps: pashto, fa: persian, pl: polish, pt: portuguese, pa: punjabi, ro: romanian, ru: russian, sm: samoan, gd: scots gaelic, sr: serbian, st: sesotho, sn: shona, sd: sindhi, si: sinhala, sk: slovak, sl: slovenian, so: somali, es: spanish, su: sundanese, sw: swahili, sv: swedish, tg: tajik, ta: tamil, te: telugu, th: thai, tr: turkish, uk: ukrainian, ur: urdu, ug: uyghur, uz: uzbek, vi: vietnamese, cy: welsh, xh: xhosa, yi: yiddish, yo: yoruba, zu: zulu", inline=False)
             em.color = 0x22BBFF
             await ctx.send(embed=em)
             return
         query = " ".join(query)
         language = language.lower()
+		if(language=="chinese_traditional"):
+			language = "chinese (traditional)"
+		elif(language=="chinese_simplified"):
+			language = "chinese (simplified)"
         if not language in googletrans.LANGUAGES:
             if(language in googletrans.LANGUAGES.values()):
                 for key, val in googletrans.LANGUAGES.items():
@@ -70,7 +74,7 @@ class Google(commands.Cog):
             prediction = translator.detect(query)
             em = discord.Embed()
             em.title = f'Translation'
-            em.description = f'Source language: {googletrans.LANGUAGES.get(prediction.lang).title()}\nConfidence: {prediction.confidence*100}%'
+            em.description = f'Source language: {googletrans.LANGUAGES.get(prediction.lang)}\nConfidence: {prediction.confidence*100}%'
             if(len(translated.text) > 1020): #1024 character limit
                 em.add_field(name="Translation", value=translated.text[:1020], inline=False)
                 em.add_field(name="\u200b", value=translated.text[1020:], inline=False)
@@ -87,13 +91,17 @@ class Google(commands.Cog):
             em.description = f'Translates TO_TRANSLATE in GIVEN LANG to RESULT_LANG using google translate'
             em.add_field(name="Example", value="/translate english french Je ne sais pas | /translate ar en hello world!", inline=False)
             em.add_field(name="Aliases", value="/t2f", inline=False)
-            em.add_field(name="Supported Languages", value="af: afrikaans, sq: albanian, am: amharic, ar: arabic, hy: armenian, az: azerbaijani, eu: basque, be: belarusian, bn: bengali, bs: bosnian, bg: bulgarian, ca: catalan, ceb: cebuano, ny: chichewa, zh-cn: chinese (simplified), zh-tw: chinese (traditional), co: corsican, hr: croatian, cs: czech, da: danish, nl: dutch, en: english, eo: esperanto, et: estonian, tl: filipino, fi: finnish, fr: french, fy: frisian, gl: galician, ka: georgian, de: german, el: greek, gu: gujarati, ht: haitian creole, ha: hausa, haw: hawaiian, iw: hebrew, he: hebrew, hi: hindi, hmn: hmong, hu: hungarian, is: icelandic, ig: igbo, id: indonesian, ga: irish, it: italian, ja: japanese, jw: javanese, kn: kannada, kk: kazakh, km: khmer, ko: korean, ku: kurdish (kurmanji)", inline=False)
+            em.add_field(name="Supported Languages", value="af: afrikaans, sq: albanian, am: amharic, ar: arabic, hy: armenian, az: azerbaijani, eu: basque, be: belarusian, bn: bengali, bs: bosnian, bg: bulgarian, ca: catalan, ceb: cebuano, ny: chichewa, zh-cn: chinese_simplified, zh-tw: chinese_traditional, co: corsican, hr: croatian, cs: czech, da: danish, nl: dutch, en: english, eo: esperanto, et: estonian, tl: filipino, fi: finnish, fr: french, fy: frisian, gl: galician, ka: georgian, de: german, el: greek, gu: gujarati, ht: haitian creole, ha: hausa, haw: hawaiian, iw: hebrew, he: hebrew, hi: hindi, hmn: hmong, hu: hungarian, is: icelandic, ig: igbo, id: indonesian, ga: irish, it: italian, ja: japanese, jw: javanese, kn: kannada, kk: kazakh, km: khmer, ko: korean, ku: kurdish (kurmanji)", inline=False)
             em.add_field(name="\u200b", value="ky: kyrgyz, lo: lao, la: latin, lv: latvian, lt: lithuanian, lb: luxembourgish, mk: macedonian, mg: malagasy, ms: malay, ml: malayalam, mt: maltese, mi: maori, mr: marathi, mn: mongolian, my: myanmar (burmese), ne: nepali, no: norwegian, or: odia, ps: pashto, fa: persian, pl: polish, pt: portuguese, pa: punjabi, ro: romanian, ru: russian, sm: samoan, gd: scots gaelic, sr: serbian, st: sesotho, sn: shona, sd: sindhi, si: sinhala, sk: slovak, sl: slovenian, so: somali, es: spanish, su: sundanese, sw: swahili, sv: swedish, tg: tajik, ta: tamil, te: telugu, th: thai, tr: turkish, uk: ukrainian, ur: urdu, ug: uyghur, uz: uzbek, vi: vietnamese, cy: welsh, xh: xhosa, yi: yiddish, yo: yoruba, zu: zulu", inline=False)
             em.color = 0x22BBFF
             await ctx.send(embed=em)
             return
         query = " ".join(query)
         language = language.lower()
+		if(language=="chinese_traditional"):
+			language = "chinese (traditional)"
+		elif(language=="chinese_simplified"):
+			language = "chinese (simplified)"
         if not language in googletrans.LANGUAGES:
             if(language in googletrans.LANGUAGES.values()):
                 for key, val in googletrans.LANGUAGES.items():
@@ -124,7 +132,7 @@ class Google(commands.Cog):
             prediction = translator.detect(query)
             em = discord.Embed()
             em.title = f'Translation'
-            em.description = f'Source language: {googletrans.LANGUAGES.get(prediction.lang).title()}\nConfidence: {prediction.confidence*100}%'
+            em.description = f'Source language: {googletrans.LANGUAGES.get(prediction.lang)}\nConfidence: {prediction.confidence*100}%'
             if(len(translated.text) > 1020): #1024 character limit
                 em.add_field(name="Translation", value=translated.text[:1020], inline=False)
                 em.add_field(name="\u200b", value=translated.text[1020:], inline=False)
